b7bd9aa3e98d39b0c84fdd0f597dcbfcc82d2eb6
Italo Santos <italosantoscont@gmail.com>
2025-11-07 15:40:23 -0300

    Jefferson e Bruno
    routes da web atualizadas para produtos e eventos

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaginaController;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\EventoController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [AuthController::class, 'index'])->name('login.form');
 
-Route::get('/paginainicial', function(){
-    return view('paginainicial');
-})->name('paginainicial');
+Route::get('/paginainicial', [PaginaController::class, 'index'])->name('paginainicial');
 
+Route::get('/sobre', [PaginaController::class, 'sobre'])->name('sobrenos');
 
-Route::get('/sobre', function(){
-    return view('sobrenos');
-})->name('sobrenos');
+Route::get('/contato', [PaginaController::class, 'contato'])->name('contato');
 
-Route::get('/contato', function(){
-    return view('contato');
-})->name('contato');
+Route::get('/eventos', [EventoController::class, 'index'])->name('evento');
 
-Route::get('/eventos', function(){
-    return view('eventos');
-})->name('evento');
+Route::get('/produtos', [ProdutoController::class, 'index'])->name('produtos');
 
-Route::get('/produtos', function(){
-    return view('produtos');
-})->name('produtos');
+?>
+
